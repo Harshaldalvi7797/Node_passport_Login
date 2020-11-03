8ca2dc84ab69091c7d5e3491326329dad924bf71
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-03 13:59:27 +0530

    commit : mongoose connecton done and user schema

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
+
+mongoose
+  .connect("mongodb://localhost/pass", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log(`connected successfully`))
+  .catch(error => console.log(`something went to wrong ${error.message}`));
 //EJS
-app.use(expressLayouts)
-app.set('view engine', "ejs")
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
 //Routes
 app.use("/", require("./routes/index"));
