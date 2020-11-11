b36cbc8e05208c30b7715e39463481d5220ec66a
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-12 00:59:47 +0530

    commit : validation done but ejs not working

--- app.js
+++ app.js
@@ -3,23 +3,17 @@ const app = express();
 const port = process.env.port || 3000;
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const ejsLint = require("ejs-lint");
-const ejs = require("ejs");
-
-// mongoose
-//   .connect("mongodb://localhost/pass", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => console.log(`connected successfully`))
-//   .catch(error => console.log(`something went to wrong ${error.message}`));
+//const ejsLint = require("ejs-lint");
+//const expressLayouts=require
+//const ejs = require("ejs");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const db = require("./config/keys").mongoURI;
 
 //connect to mongoose
-
 mongoose
-  .connect(db, { useNewUrlParser: true,useUnifiedTopology: true  })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongodb connected"))
   .catch(err => console.log(err));
 
@@ -29,6 +23,26 @@ app.set("view engine", "ejs");
 
 //Bodyparser
 app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//connect flash
+app.use(flash());
+
+//global vars
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //Routes
 app.use("/", require("./routes/index"));
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const passport = require("./");
+const bcrypt = require("bcrypt");
+//const name = require("../views/login");
 //welcome
 router.get("/", (req, res) => res.render("welcome"));
 
@@ -17,44 +19,54 @@ router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
-  // if (errors.length > 0) {
-  //   res.render("register", {
-  //     errors,
-  //     name,
-  //     email,
-  //     password,
-  //     password2
-  //   });
-  // } else {
-  //   User.findOne({ email: email }).then(user => {
-  //     if (user) {
-  //       errors.push({ msg: "Email already esist" });
-
-  //       res.render("register", { errors, name, email, password, password2 });
-  //     }
-  //   });
-  // }
-  // else
-
-  //   console.log(newUser)
-  //   res.send("hello")
-  // }  {
-  //   const newUser = new User({
-  //     name,email,password
-  //   })
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      }
+    });
+  }
   const newUser = new User({
     name,
     email,
     password
   });
-  res.send("hello");
-  console.log(req.body);
 
-  newUser
-    .save()
-    .then(user => {
-      req.flash("success_msg", "You are now registered and can log in");
-      res.redirect("/users/login");
-    })
-    .catch(err => console.log(err));
+  //Hash password
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+
+        .then(user => {
+          //req.flash("success_msg", "You are now register");
+          res.redirect("login");
+        })
+        .catch(err => console.log(err));
+    });
+  });
+
+  // res.send("hello");
+  console.log(req.body);
+  console.log(newUser);
+
+  // newUser
+  //   .save()
+  //   .then(user => {
+  //     req.flash("success_msg", "You are now registered and can log in");
+  //     res.redirect("/login");
+  //   })
+  //   .catch(err => console.log(err));
 });
